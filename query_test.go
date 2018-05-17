ef57a337cd1b2f9a20aaf104a17bacf9de82bde4
Frederick-S <mao_xiaodan@hotmail.com>
2018-05-17 13:50:44 +0800

    Update test

--- query_test.go
+++ query_test.go
@@ -9,3 +9,11 @@ func TestQueryWord(t *testing.T) {
 		t.Error("Query was incorrect, cannot find explanations for book")
 	}
 }
+
+func TestQueryNotFound(t *testing.T) {
+	word := query("Arthimetic")
+
+	if len(word.suggestions) == 0 {
+		t.Error("Query was incorrect, should return suggestions")
+	}
+}
